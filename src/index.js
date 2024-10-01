18c229a168c7b14b208002d8b37167c1907816b8
Saalim Aqueel <aqueel192@gmail.com>
2024-10-01 08:02:15 +0530

    application setup [app.js]

--- src/index.js
+++ src/index.js
@@ -1,28 +1,18 @@
 // require("dotenv").config({ path: "/.env" });
 import dotenv from "dotenv";
-import express from "express";
 import connectDB from "./db/db.js";
+import { app } from "./app.js";
 dotenv.config({ path: "/.env" });
 
-const app = express();
-
 connectDB()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("DB connection failed", err);
+  });
 
 /*
 (async () => {
